02dc22506e0d639f2c18e97567ef4d6f3ae7c52b
bchincho <braianchincho@gmail.com.com>
2025-09-16 19:34:53 -0300

    Cors in get users

--- app/controllers/users.controllers.js
+++ app/controllers/users.controllers.js
@@ -3,6 +3,9 @@ import createRandomUser from "../services/user.service.js";
 export default class UserController {
 
     getUsers(req, res) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
         const page = parseInt(req.query.page) || 1;
         const limit = parseInt(req.query.limit) || 10;
         const totalUsers = 500;
